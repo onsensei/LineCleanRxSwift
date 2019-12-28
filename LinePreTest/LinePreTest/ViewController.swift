c828289721299b28af2b42b47c3638b0de29f169
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-12-28 12:23:40 +0700

    init

---
+++ LinePreTest/LinePreTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinePreTest
+//
+//  Created by Thanaphat Suwannikornkul on 28/12/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LinePreTest/LinePreTestTests/LinePreTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  LinePreTestTests.swift
+//  LinePreTestTests
+//
+//  Created by Thanaphat Suwannikornkul on 28/12/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import XCTest
+@testable import LinePreTest
+
+class LinePreTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
