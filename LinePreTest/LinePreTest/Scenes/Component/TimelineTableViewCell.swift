fb284e12a0ae74a5d9bfcd93f7f1c4118f2e1ef5
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-12-29 16:14:26 +0700

    move files

--- LinePreTest/LinePreTest/Scenes/Component/TimelineTableViewCell.swift
+++ LinePreTest/LinePreTest/Scenes/Component/TimelineTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TimelineTableViewCell: UITableViewCell {
+class TimelineTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var photoCollectionView: UICollectionView!
   
@@ -29,23 +29,44 @@ class TimelineTableViewCell: UITableViewCell {
   
   // MARK: Do something
   
+  var datasource: PostAlbum = PostAlbum(title: "", photos: [])
+  
   func displayCell(postAlbum: PostAlbum)
   {
-    titleLabel.text = postAlbum.title
-    titleLabel.text = "\(postAlbum.title) (\(postAlbum.photos.count))"
+    datasource = postAlbum
     
-    if (postAlbum.photos.count == 0) {
+    titleLabel.text = datasource.title
+    titleLabel.text = "\(datasource.title) (\(datasource.photos.count))"
+    
+    if (datasource.photos.count == 0) {
       photoZeroHeightConstraint.isActive = true
       photoSquareRatioConstraint.isActive = false
       photoWideRatioConstraint.isActive = false
-    } else if (postAlbum.photos.count == 1) {
+    } else if (datasource.photos.count == 1) {
       photoZeroHeightConstraint.isActive = false
       photoSquareRatioConstraint.isActive = false
       photoWideRatioConstraint.isActive = true
-    } else if (postAlbum.photos.count > 1) {
+    } else if (datasource.photos.count > 1) {
       photoZeroHeightConstraint.isActive = false
       photoSquareRatioConstraint.isActive = true
       photoWideRatioConstraint.isActive = false
     }
+  }
+  
+  // MARK: UICollectionViewDataSource
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return datasource.photos.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    // get a reference to our storyboard cell
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+
+    // Use the outlet in our custom class to get a reference to the UILabel in the cell
+    cell.myLabel.text = self.items[indexPath.item]
+    cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+
+    return cell
   }
 }
