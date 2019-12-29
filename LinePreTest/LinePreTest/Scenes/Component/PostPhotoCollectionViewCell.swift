80a613cf431a9b7d32f80d1f94d3bdde514cfd4b
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-12-29 16:14:51 +0700

    init new comp

---
+++ LinePreTest/LinePreTest/Scenes/Component/PostPhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostPhotoCollectionViewCell.swift
+//  LinePreTest
+//
+//  Created by Thanaphat Suwannikornkul on 29/12/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+
+class PostPhotoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
