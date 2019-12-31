c801a55d5f9abcf8488e80afa1891ef68b9da6a4
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-12-31 17:04:00 +0700

    new files for post cell

---
+++ LinePreTest/LinePreTest/Components/PostMessageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostMessageTableViewCell.swift
+//  LinePreTest
+//
+//  Created by Thanaphat Suwannikornkul on 31/12/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+
+class PostMessageTableViewCell: UITableViewCell {
+  @IBOutlet weak var titleLabel: UILabel!
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+
+    // Configure the view for the selected state
+  }
+}
