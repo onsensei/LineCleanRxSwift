208dda6ab2fd0118f167e648c513f1ac10250a06
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-12-31 10:30:08 +0700

    add files for cell

---
+++ LinePreTest/LinePreTest/Components/AddPhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AddPhotoCollectionViewCell.swift
+//  LinePreTest
+//
+//  Created by Thanaphat Suwannikornkul on 31/12/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ LinePreTest/LinePreTest/Components/DummyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DummyCollectionViewCell.swift
+//  LinePreTest
+//
+//  Created by Thanaphat Suwannikornkul on 31/12/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+
+class DummyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
